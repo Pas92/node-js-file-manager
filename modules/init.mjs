61836c75dca11bf64bf6471928078f99a3a89f64
Pas92 <pavelburdavitsyn92@gmail.com>
2022-12-18 19:17:55 +0300

    fix: fix app initialization

--- modules/init.mjs
+++ modules/init.mjs
@@ -1,19 +1,21 @@
-import * as fs from 'node:fs/promises';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import { appEnv } from './app-enviroment.mjs';
 import { parseCommand } from './cli-parser.mjs';
 import { ARGS, COLOR } from './constants.mjs';
 import { setUserDefaultPath } from './path-parser.mjs';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const setUserName = () => {
   const argument = process.argv.filter((arg) =>
     arg.startsWith(`${ARGS.user}`)
   )[0];
-  appEnv.user = argument.split('=')[1];
+
+  if (!argument) {
+    console.log(
+      `${COLOR.red}Invalid input! Enter the username with argument ${COLOR.magenta}${ARGS.user}=<username>${COLOR.default}`
+    );
+    process.exit();
+  } else {
+    appEnv.user = argument.split('=')[1];
+  }
 };
 
 export const appInit = async () => {
@@ -21,10 +23,10 @@ export const appInit = async () => {
   setUserDefaultPath();
 
   process.stdout.write(
-    `Welcome to the File Manager, ${COLOR.yellow}${appEnv.user}!${COLOR.default}\n`
+    `Welcome to the File Manager, ${COLOR.yellow}${appEnv.user}${COLOR.default}!\n`
   );
   process.stdout.write(
-    `You are currently in ${COLOR.yellow}${appEnv.currentPath}!${COLOR.default}\n`
+    `You are currently in ${COLOR.yellow}${appEnv.currentPath}${COLOR.default}\n`
   );
 
   await parseCommand();
